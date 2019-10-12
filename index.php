07522604f32cff66b62365afc5e75603401253a7
Nick Kelley <nick@nickkelleystuff.com>
2019-10-12 16:59:02 -0700

    add posts loop to index.php

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php get_header(); ?>
-<?php get_footer(); ?>
+<?php
+get_header();
+if ( have_posts() ) :
+  while ( have_posts() ) : the_post();
+    // Display post content ?>
+    <article>
+      <h2><?php the_title(); ?></h2><?php
+      the_content(); ?>
+    </article><?php
+  endwhile;
+endif;
+get_footer();
