e58236b50bc10f38b180b9f642c65224ff412000
Nick Kelley <nick@nickkelleystuff.com>
2019-10-11 22:17:38 -0700

    add title to loop

--- home.php
+++ home.php
@@ -2,7 +2,8 @@
 get_header();
 if ( have_posts() ) :
     while ( have_posts() ) : the_post();
-        // Display post content
+        // Display post content ?>
+        <h2><?php the_title(); ?></h2><?php
         the_content();
     endwhile;
 endif;
