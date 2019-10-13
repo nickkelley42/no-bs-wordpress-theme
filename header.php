6b0ec143249f3f9e018748f137b874f6de89f372
Nick Kelley <nick@nickkelleystuff.com>
2019-10-12 17:14:54 -0700

    tweak header meta shit

--- header.php
+++ header.php
@@ -1,7 +1,9 @@
 <!doctype html>
 <html>
-<head>
-  <title><?php bloginfo('name'); ?>-<?php the_title() ?></title>
+<head <?php languate_attributes(); ?>>
+  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <?php wp_head(); ?>
 </head>
 <body>
 <header>
