e5a5cd55983a7b359294d4011f2c92630a930998
Nick Kelley <nick@nickkelleystuff.com>
2019-10-12 17:49:05 -0700

    first attempt at removing bs from header

--- functions.php
+++ functions.php
@@ -2,3 +2,22 @@
 
 // make wordpress supply the title-tag
 add_theme_support( 'title-tag' );
+
+function clear_styles_and_scripts() {
+  global $wp_scripts;
+  global $wp_styles;
+  $styles_to_keep = array(
+    "wp-admin",
+    "admin-bar",
+    "dashicons",
+    "open-sans"
+  );
+  
+  foreach( $wp_styles ->queue as $handle ) {
+    if ( in_array($handle, $styles_to_keep) ) continue;
+    wp_dequeue_style( $handle );
+    wp_deregister_style( $handle );
+  
+  }
+}
+add_action( 'wp_enqueue_scripts', 'clear_styles_and_scripts', 100 );
