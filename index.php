2c5ff19292b997d938f3c73afe0bd8cb22bf7917
Nick Kelley <nick@nickkelleystuff.com>
2019-10-12 17:42:04 -0700

    forgot semicolon

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if ( have_posts() ) {
       <main>
         <?php
           the_title('<h2>', '</h2>');
-          echo "<p>Posted by "
+          echo "<p>Posted by ";
           the_author();
           echo "</p>";
           the_content();
