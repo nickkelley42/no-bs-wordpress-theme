9b3ccd4a5121880da2d21783e69e3c06d98b8737
Nick Kelley <nick@nickkelleystuff.com>
2019-10-12 17:15:26 -0700

    another typo fix

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html>
-<head <?php languate_attributes(); ?>>
+<head <?php language_attributes(); ?>>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <?php wp_head(); ?>
