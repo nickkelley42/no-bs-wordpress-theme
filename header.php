22bf5370a42c11ab70c7c012faa31ceddfb80b28
Nick Kelley <nick@nickkelleystuff.com>
2019-10-12 18:09:31 -0700

    add wp_head back into header

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head <?php language_attributes(); ?>>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <?php /*wp_head(); */ ?>
+  <?php wp_head(); ?>
 </head>
 <body>
 <header>
