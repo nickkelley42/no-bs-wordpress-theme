fd5c4dff60a56964e05b1b79155142ea27005372
Nick Kelley <nick@nickkelleystuff.com>
2019-10-12 17:09:21 -0700

    Tweak index.php

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
 get_header();
-if ( have_posts() ) :
-  while ( have_posts() ) : the_post();
+if ( have_posts() ) {
+  while ( have_posts() ) {
+    the_post();
     // Display post content ?>
-    <article>
-      <h2><?php the_title(); ?></h2><?php
-      the_content(); ?>
-    </article><?php
-  endwhile;
-endif;
+    <section>
+      <main>
+        <h2><?php the_title(); ?></h2><?php
+        the_content(); ?>
+      </main>
+    </section>
+  }
+}
 get_footer();
