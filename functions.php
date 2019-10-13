080ad5fe8d8842788f1189d08957943c9bf82c17
Nick Kelley <nick@nickkelleystuff.com>
2019-10-12 17:50:25 -0700

    fuck it blow it all up

--- functions.php
+++ functions.php
@@ -3,14 +3,16 @@
 // make wordpress supply the title-tag
 add_theme_support( 'title-tag' );
 
+// Remove as much of the wp_head bullshit as possible while
+// still keeping it useful.
 function clear_styles_and_scripts() {
   global $wp_scripts;
   global $wp_styles;
   $styles_to_keep = array(
-    "wp-admin",
-    "admin-bar",
-    "dashicons",
-    "open-sans"
+//    "wp-admin",
+//    "admin-bar",
+//    "dashicons",
+//    "open-sans"
   );
   
   foreach( $wp_styles ->queue as $handle ) {
